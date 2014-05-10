7014ed0480327ba0966517a634e7e328e723f66b
Danilo de Jesus da Silva Bellini <danilo.bellini@gmail.com>
2014-05-09 21:34:50 -0300

    Add Danilo (me) to the copyright message

--- hipsterplot.py
+++ hipsterplot.py
@@ -3,7 +3,8 @@
 #
 # The MIT License (MIT)
 #
-# Copyright (c) 2014 Ian Horn <horn.imh@gmail.com>
+# Copyright (c) 2014 Ian Horn <horn.imh@gmail.com> and
+#                    Danilo J. S. Bellini <danilo.bellini@gmail.com>
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
